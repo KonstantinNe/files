411735b2ea3a069474e312bd305d0f597259bbcb
Konstantin <nemeshkv@gmail.com>
2025-08-21 01:57:50 +0500

    модуль2_1

--- src/ru/nemesh/Main.java
+++ src/ru/nemesh/Main.java
@@ -7,9 +7,32 @@ public class Main {
 
     public static void main(String[] args) {
         String result = null;
-        String a = "D:\\test";
+        //String username = System.getenv("Katy");
+        //String password = System.getenv("");
+        String a = "D:\\simplexcel.xls";
+     //   Scanner scanner = new Scanner(System.in);
+        //System.out.print("¬ведите им€ пользовател€: ");
+        //String username = scanner.nextLine();
+
+        //System.out.print("¬ведите им€ пароль: ");
+        //String password = scanner.nextLine();
+
         File file = new File(a);
         if (file.exists()) {
+            String fileName = file.getName();
+            String nameWithoutExtension = null;
+            String extension = null;
+
+            int dotIndex = fileName.lastIndexOf('.');
+
+            if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
+                nameWithoutExtension = fileName.substring(0, dotIndex);
+                extension = fileName.substring(dotIndex + 1);
+            } else {
+                nameWithoutExtension = fileName;
+                extension = "";
+            }
+
             result = Check.getCheck(a);
         } else {
             System.out.println("‘айл не найден");
