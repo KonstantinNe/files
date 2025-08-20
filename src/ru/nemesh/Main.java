c733c84e5695aa41715e6f8a097f4e8cb59aa06b
Konstantin <nemeshkv@gmail.com>
2025-08-21 00:45:52 +0500

    модуль2

--- src/ru/nemesh/Main.java
+++ src/ru/nemesh/Main.java
@@ -1,16 +1,20 @@
 package ru.nemesh;
 
 
+import java.io.File;
 
 public class Main {
 
-    public static void main(String [] args){
-        String a = null;
-        String result;
-        a = "D:\\simplexcel.xls";
-        result = Check.getCheck(a);
+    public static void main(String[] args) {
+        String result = null;
+        String a = "D:\\test";
+        File file = new File(a);
+        if (file.exists()) {
+            result = Check.getCheck(a);
+        } else {
+            System.out.println("Файл не найден");
+            //       System.out.println(ExcelParser.parse("D:\\simplexcel.xls"));
+        }
         System.out.println(result);
- //       System.out.println(ExcelParser.parse("D:\\simplexcel.xls"));
     }
-
 }
